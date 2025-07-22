b4883fd374915301313e544a317af4795641f005
Vyacheslav <nopisem@yandex.ru>
2025-07-22 11:13:41 +0700

    update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -13,13 +13,14 @@ export default defineConfigWithVueTs(
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-  {
-    rules: {
-        'vue/multi-word-component-names': 0,
-      },
-  },
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
   skipFormatting,
 )
+
