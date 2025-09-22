b6cce2740b3dd27244ee6e2b0eb181c3871644e8
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2025-09-22 12:26:49 +0700

    added formkit, updated layout - added create task button

--- src/components/Layout/main/AuthLayout.vue
+++ src/components/Layout/main/AuthLayout.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 const { pageData } = storeToRefs(usePageStore())
+
+const taskSheetOpen = ref(false)
 </script>
 
 <template>
-  <Sidebar />
+  <Sidebar @task-clicked="taskSheetOpen = true"/>
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
     <TopNavbar />
-
+    <AppNewTask v-model="taskSheetOpen"/>
     <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
       <div class="flex items-center">
         <h1 class="text-lg font-semibold md:text-2xl">{{  pageData.title }}</h1>
